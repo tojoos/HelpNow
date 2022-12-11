c468b40304f85dca0638776d74c871ac16fb43d9
ToJoos <joosx2000@gmail.com>
2022-12-11 20:55:49 +0100

    Added Statistic service, and REST controller to communicate with frontend

--- src/main/java/dev/tojoos/helpnow/controllers/StatisticsController.java
+++ src/main/java/dev/tojoos/helpnow/controllers/StatisticsController.java
@@ -1,2 +1,30 @@
-package dev.tojoos.helpnow.controllers;public class StatisticsController {
+package dev.tojoos.helpnow.controllers;
+
+import dev.tojoos.helpnow.model.Statistics;
+import dev.tojoos.helpnow.services.StatisticsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/statistics")
+public class StatisticsController {
+    private final StatisticsService statisticsService;
+
+    public StatisticsController(StatisticsService StatisticsService) {
+        this.statisticsService = StatisticsService;
+    }
+
+    @GetMapping("/getStats")
+    public ResponseEntity<Statistics> getStatistics() {
+        Statistics statistics = statisticsService.getById(1L);
+        return new ResponseEntity<>(statistics, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Statistics> updateOrganization(@RequestBody Statistics statistics) {
+        Statistics updatedStatistics = statisticsService.update(statistics);
+        return new ResponseEntity<>(updatedStatistics, HttpStatus.OK);
+    }
 }
--- src/main/java/dev/tojoos/helpnow/services/StatisticsService.java
+++ src/main/java/dev/tojoos/helpnow/services/StatisticsService.java
@@ -1,2 +1,8 @@
-package dev.tojoos.helpnow.services;public interface StatisticsService {
+package dev.tojoos.helpnow.services;
+
+
+import dev.tojoos.helpnow.model.Statistics;
+
+public interface StatisticsService extends CrudService<Statistics> {
+    void recalculateStatistics();
 }
