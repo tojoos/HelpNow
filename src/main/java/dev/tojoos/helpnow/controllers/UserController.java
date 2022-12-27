898887901a21a2223a0a924a293f30905b872259
ToJoos <joosx2000@gmail.com>
2022-12-27 22:57:18 +0100

    Implemented user repo, service and controller. Added JWT dependency

--- src/main/java/dev/tojoos/helpnow/controllers/UserController.java
+++ src/main/java/dev/tojoos/helpnow/controllers/UserController.java
@@ -1,13 +1,25 @@
 package dev.tojoos.helpnow.controllers;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.tojoos.helpnow.model.Role;
 import dev.tojoos.helpnow.model.User;
-import dev.tojoos.helpnow.services.UserService;
 import dev.tojoos.helpnow.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/user")
@@ -46,5 +58,59 @@ public class UserController {
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/role/add")
+    public ResponseEntity<Role> addRole(@RequestBody Role role) {
+        Role savedRole = userService.addRole(role);
+        return new ResponseEntity<>(savedRole, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody String username, @RequestBody String roleName) {
+        userService.addRoleToUser(username, roleName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) { //it is token auth
+            try {
+                String refreshToken = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes()); //same secret that encoded the refreshToken
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refreshToken);
+                String username = decodedJWT.getSubject();
+                User foundUser = userService.getByUsername(username);
+
+                String accessToken = JWT.create()
+                        .withSubject(foundUser.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000)) //10 min refreshToken
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", foundUser.getRoles().stream().map(Role::getName).toList())
+                        .sign(algorithm);
+
+
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", accessToken);
+                tokens.put("refresh_token", refreshToken);
+
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+
+            } catch (Exception ex) {
+                response.setHeader("error", ex.getMessage());
+                response.setStatus(FORBIDDEN.value());
+
+                Map<String, String> error = new HashMap<>();
+                error.put("error_message", ex.getMessage());
+
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        } else {
+            throw new RuntimeException("Refresh token is missing.");
+        }
     }
 }
