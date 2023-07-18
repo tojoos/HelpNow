3bc8bd8382a1f64d7508512c0ec942f4e8f798cc
ToJoos <joosx2000@gmail.com>
2023-07-18 19:40:24 +0200

    Refactor a exceptions to meet linting requirements

--- src/main/java/dev/tojoos/helpnow/exceptions/DefaultExceptionHandler.java
+++ src/main/java/dev/tojoos/helpnow/exceptions/DefaultExceptionHandler.java
@@ -1,5 +1,8 @@
 package dev.tojoos.helpnow.exceptions;
 
+import java.time.LocalDateTime;
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,47 +11,50 @@ import org.springframework.security.authentication.InsufficientAuthenticationExc
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.persistence.EntityNotFoundException;
-import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
-
+/**
+ * Default exception handler for the REST apis.
+ *
+ * @author Jan Olszówka
+ * @version 1.0
+ * @since 2023-05-06
+ */
 @Slf4j
 @ControllerAdvice
 public class DefaultExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public final ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
-        ApiError apiError = new ApiError(
-                request.getRequestURI(),
-                HttpStatus.NOT_FOUND,
-                LocalDateTime.now(),
-                ex.getMessage()
-        );
+  @ExceptionHandler(EntityNotFoundException.class)
+  public final ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, HttpServletRequest request) {
+    ApiError apiError = new ApiError(
+        request.getRequestURI(),
+        HttpStatus.NOT_FOUND,
+        LocalDateTime.now(),
+        ex.getMessage()
+    );
 
-        return new ResponseEntity<>(apiError, apiError.status());
-    }
+    return new ResponseEntity<>(apiError, apiError.status());
+  }
 
-    @ExceptionHandler(InsufficientAuthenticationException.class)
-    public final ResponseEntity<Object> handleInsufficientAuthenticationException(InsufficientAuthenticationException ex, HttpServletRequest request) {
-        ApiError apiError = new ApiError(
-                request.getRequestURI(),
-                HttpStatus.UNAUTHORIZED,
-                LocalDateTime.now(),
-                ex.getMessage()
-        );
+  @ExceptionHandler(InsufficientAuthenticationException.class)
+  public final ResponseEntity<Object> handleInsufficientAuthenticationException(InsufficientAuthenticationException ex, HttpServletRequest request) {
+    ApiError apiError = new ApiError(
+        request.getRequestURI(),
+        HttpStatus.UNAUTHORIZED,
+        LocalDateTime.now(),
+        ex.getMessage()
+    );
 
-        return new ResponseEntity<>(apiError, apiError.status());
-    }
+    return new ResponseEntity<>(apiError, apiError.status());
+  }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public final ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex, HttpServletRequest request) {
-        ApiError apiError = new ApiError(
-                request.getRequestURI(),
-                HttpStatus.UNAUTHORIZED,
-                LocalDateTime.now(),
-                ex.getMessage()
-        );
+  @ExceptionHandler(BadCredentialsException.class)
+  public final ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex, HttpServletRequest request) {
+    ApiError apiError = new ApiError(
+        request.getRequestURI(),
+        HttpStatus.UNAUTHORIZED,
+        LocalDateTime.now(),
+        ex.getMessage()
+    );
 
-        return new ResponseEntity<>(apiError, apiError.status());
-    }
+    return new ResponseEntity<>(apiError, apiError.status());
+  }
 }
