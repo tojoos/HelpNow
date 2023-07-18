f3ab3271d538a619e428ae7f9216147b5882f75a
ToJoos <joosx2000@gmail.com>
2023-07-18 20:00:33 +0200

    Refactor a security classes to meet linting requirements

--- src/main/java/dev/tojoos/helpnow/security/SecurityConfiguration.java
+++ src/main/java/dev/tojoos/helpnow/security/SecurityConfiguration.java
@@ -1,15 +1,15 @@
 package dev.tojoos.helpnow.security;
 
+import static org.springframework.http.HttpMethod.*;
+
 import dev.tojoos.helpnow.filters.CustomAuthenticationFilter;
 import dev.tojoos.helpnow.filters.CustomAuthorizationFilter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,75 +20,80 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
-
-import static org.springframework.http.HttpMethod.*;
-
-@Configuration @EnableWebSecurity @RequiredArgsConstructor
+/**
+ * Security configuration class. It configures cors, access to several
+ * application's mappings, authenticate users.
+ *
+ * @author Jan Olszówka
+ * @version 1.0
+ * @since 2022-12-27
+ */
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  private final UserDetailsService userDetailsService;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        customAuthenticationFilter.setFilterProcessesUrl("/login");
+  @Override
+  protected void configure(HttpSecurity httpSecurity) throws Exception {
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+    customAuthenticationFilter.setFilterProcessesUrl("/login");
 
-        httpSecurity.csrf().disable();
-        httpSecurity.cors();
+    httpSecurity.csrf().disable();
+    httpSecurity.cors();
 
-        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        httpSecurity.authorizeRequests().antMatchers(GET, "/fundraise/**", "/statistics/**",
+    httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    httpSecurity.authorizeRequests().antMatchers(GET, "/fundraise/**", "/statistics/**",
                 "/announcement/**", "/organization/**", "/employee/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(OPTIONS, "/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(PUT, "/statistics/**").permitAll();
-        httpSecurity.authorizeRequests().antMatchers(POST, "/user/add").permitAll();
-        httpSecurity.authorizeRequests().antMatchers("/login", "/user/token/refresh", "/mail/contact").permitAll();
+    httpSecurity.authorizeRequests().antMatchers(OPTIONS, "/**").permitAll();
+    httpSecurity.authorizeRequests().antMatchers(PUT, "/statistics/**").permitAll();
+    httpSecurity.authorizeRequests().antMatchers(POST, "/user/add").permitAll();
+    httpSecurity.authorizeRequests().antMatchers("/login", "/user/token/refresh", "/mail/contact").permitAll();
 
-        //user accesses
-        httpSecurity.authorizeRequests().antMatchers(POST, "/announcement/**").hasAnyAuthority("USER", "ADMIN");
-        httpSecurity.authorizeRequests().antMatchers(PUT, "/fundraise/**").hasAnyAuthority("USER", "ADMIN");
-        httpSecurity.authorizeRequests().antMatchers(GET, "/employee/**").hasAnyAuthority("ADMIN");
+    //user accesses
+    httpSecurity.authorizeRequests().antMatchers(POST, "/announcement/**").hasAnyAuthority("USER", "ADMIN");
+    httpSecurity.authorizeRequests().antMatchers(PUT, "/fundraise/**").hasAnyAuthority("USER", "ADMIN");
+    httpSecurity.authorizeRequests().antMatchers(GET, "/employee/**").hasAnyAuthority("ADMIN");
 
-        //admin accesses here
-        httpSecurity.authorizeRequests().antMatchers(POST, "/statistics/**",
+    //admin accesses here
+    httpSecurity.authorizeRequests().antMatchers(POST, "/statistics/**",
                  "/organization/**", "/employee/**", "/user/**").hasAnyAuthority("ADMIN");
-        httpSecurity.authorizeRequests().antMatchers(PUT,
+    httpSecurity.authorizeRequests().antMatchers(PUT,
                 "/announcement/**", "/organization/**", "/employee/**", "/user/**").hasAnyAuthority("ADMIN");
-        httpSecurity.authorizeRequests().antMatchers(DELETE,"/employee/**").hasAnyAuthority("ADMIN");
+    httpSecurity.authorizeRequests().antMatchers(DELETE, "/employee/**").hasAnyAuthority("ADMIN");
 
-        httpSecurity.authorizeRequests().anyRequest().authenticated();
+    httpSecurity.authorizeRequests().anyRequest().authenticated();
 
-        httpSecurity.addFilter(customAuthenticationFilter);
-        httpSecurity.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+    httpSecurity.addFilter(customAuthenticationFilter);
+    httpSecurity.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+  }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(List.of("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+  }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+    configuration.setAllowedMethods(List.of("HEAD",
+        "GET", "POST", "PUT", "DELETE", "PATCH"));
+    // setAllowCredentials(true) is important, otherwise:
+    // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+    configuration.setAllowCredentials(true);
+    // setAllowedHeaders is important! Without it, OPTIONS preflight request
+    // will fail with 403 Invalid CORS request
+    configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 }
