c3fe62fca15c3f5e428350fbc5a66a169c43b2ca
tojoos <joosx2000@gmail.com>
2023-11-08 18:02:46 +0100

    Migrate deprecated security configuration

--- src/main/java/dev/tojoos/helpnow/security/SecurityConfiguration.java
+++ src/main/java/dev/tojoos/helpnow/security/SecurityConfiguration.java
@@ -11,10 +11,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -30,13 +30,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
  */
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
   private final UserDetailsService userDetailsService;
 
-  @Override
-  protected void configure(HttpSecurity httpSecurity) throws Exception {
-    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
     customAuthenticationFilter.setFilterProcessesUrl("/login");
 
     httpSecurity
@@ -46,37 +46,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests()
-            .antMatchers(GET, "/fundraise/**", "/statistics/**", "/announcement/**", "/organization/**", "/employee/**").permitAll()
-            .antMatchers(OPTIONS, "/**").permitAll()
-            .antMatchers(PUT, "/statistics/**").permitAll()
-            .antMatchers(POST, "/user/add").permitAll()
-            .antMatchers("/login", "/user/token/refresh", "/mail/contact").permitAll()
-            .antMatchers(POST, "/announcement/**").hasAnyAuthority("USER", "ADMIN")
-            .antMatchers(PUT, "/fundraise/**").hasAnyAuthority("USER", "ADMIN")
-            .antMatchers(GET, "/employee/**").hasAnyAuthority("ADMIN")
-            .antMatchers(POST, "/statistics/**", "/organization/**", "/employee/**", "/user/**").hasAnyAuthority("ADMIN")
-            .antMatchers(PUT, "/announcement/**", "/organization/**", "/employee/**", "/user/**").hasAnyAuthority("ADMIN")
-            .antMatchers(DELETE, "/employee/**").hasAnyAuthority("ADMIN")
-            .anyRequest().authenticated()
+        .antMatchers(GET, "/fundraise/**", "/statistics/**", "/announcement/**", "/organization/**", "/employee/**").permitAll()
+        .antMatchers(OPTIONS, "/**").permitAll()
+        .antMatchers(PUT, "/statistics/**").permitAll()
+        .antMatchers(POST, "/user/add").permitAll()
+        .antMatchers("/login", "/user/token/refresh", "/mail/contact").permitAll()
+        .antMatchers(POST, "/announcement/**").hasAnyAuthority("USER", "ADMIN")
+        .antMatchers(PUT, "/fundraise/**").hasAnyAuthority("USER", "ADMIN")
+        .antMatchers(GET, "/employee/**").hasAnyAuthority("ADMIN")
+        .antMatchers(POST, "/statistics/**", "/organization/**", "/employee/**", "/user/**").hasAnyAuthority("ADMIN")
+        .antMatchers(PUT, "/announcement/**", "/organization/**", "/employee/**", "/user/**").hasAnyAuthority("ADMIN")
+        .antMatchers(DELETE, "/employee/**").hasAnyAuthority("ADMIN")
+        .anyRequest().authenticated()
         .and()
         .addFilter(customAuthenticationFilter)
         .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-  }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService).passwordEncoder(this.bcryptPasswordEncoder());
+    return httpSecurity.build();
   }
 
   @Bean
-  public BCryptPasswordEncoder bcryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
+  public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class)
+        .userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder)
+        .and()
+        .build();
   }
 
   @Bean
